efaf2e4840de41127b1982d667f2ed5bbddfbd8a
ruchi06 <ruchi06@gmail.com>
2023-01-17 20:13:14 +0530

    Simple Linkedd List

---
+++ LinkeddList/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinkeddList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkeddList list = new LinkeddList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            
+           
+        }
+    }
+}
