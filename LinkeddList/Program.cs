83ab98ae0caacad43df7341b6bcb12530d2ba531
ruchi06 <ruchi06@gmail.com>
2023-01-18 15:31:20 +0530

    Appending LinkedList

--- LinkeddList/Program.cs
+++ LinkeddList/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             LinkeddList list = new LinkeddList();
-            list.Add(70);
+            list.Add(56);
             list.Insert_front(30);
-            list.Insert_front(56);
+            list.Insert_front(70);
             list.Display();
             
            
